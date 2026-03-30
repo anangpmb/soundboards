b096dee84dc7664ce837d06391c5395319ac86da
Anang Pambudi <anang.pambudi@accenture.com>
2026-03-30 23:04:30 +0700

    feat : initiate commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        control: resolve(__dirname, 'control.html'),
+        widget: resolve(__dirname, 'widget.html'),
+      },
+    },
+  },
+});
